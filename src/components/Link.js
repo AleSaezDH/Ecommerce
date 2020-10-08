d60c793d235663c3fc76f2f3f8b4c859af2f0853
unknown <saezfvet@gmail.com>
2020-10-07 23:21:10 -0300

    creando proyecto reacto

---
+++ src/components/Link.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Link () {
+    return <a
+    className="App-link"
+    href="https://reactjs.org"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    Learn React
+  </a>
+}
+
+export default Link;
---
+++ src/components/Text.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Text () {
+    return <p>Edit <code>src/App.js</code> and save to reload.</p>
+}
+
+export default Text;
